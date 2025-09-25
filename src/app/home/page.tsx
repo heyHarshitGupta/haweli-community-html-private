0659c46a2b0fb5b7396f052c5c1306770efba12d
heyHarshitGupta <deepsinghyadav933@gmail.com>
2025-09-26 04:56:06 +0530

    fifth commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      welcome to home page navigate to{" "}
+      <a href="https://deepsinghyadav.com">The og developer</a>
+    </div>
+  );
+};
+
+export default page;
